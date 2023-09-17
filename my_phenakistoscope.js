99bff9002cd8e071aeff85aa7b3f0ca4ce98db5b
phernados <g.e.evans0793@gmail.com>
2023-09-18 09:38:07 +1200

    final touches

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,74 +3,100 @@ const SLICE_COUNT = 10;
 function setup_pScope(pScope) {
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image("deadPhog", "png");
+  pScope.load_image("tadfog2", "png");
   pScope.load_image("phog", "png");
-
+  pScope.load_image("fly", "png");
+  // pScope.load_image_seq
 
 }
 //exprement
 
 function setup_layers(pScope) {
 
-  new PLayer(null, "#4a3194"); //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, "#396acc"); //lets us draw the whole circle background, ignoring the boundaries
   
-  var expBOUND = new PLayer(lillypad); //lillypad
+  var waterlayer = new PLayer(water); //background
+  waterlayer.set_boundary(0, 800);
+
+  var deadPhogL = new PLayer(deadPhog);
+  deadPhogL.mode(SWIRL(2));
+  deadPhogL.set_boundary(400, 1000);
+
+  var expBOUND = new PLayer(lillypad); 
   expBOUND.mode(RING);
   expBOUND.set_boundary(0, 800);
 
-  var deadPhogL = new PLayer(deadPhog);
-  deadPhogL.mode(SWIRL(1));
-  deadPhogL.set_boundary(600, 1000);
-
-  var phogL = new PLayer(phog,);
+  var phogL = new PLayer(phog);
   phogL.mode(RING);
-  phogL.set_boundary(0, 800,);
+  phogL.set_boundary(0, 800);
+
+  var expBOUND1 = new PLayer(corn); //small lillypads 
+  expBOUND1.mode(RING);
+  expBOUND1.set_boundary(0, 800);
+
+  var flyL = new PLayer(fly);
+  flyL.mode(RING);
+  flyL.set_boundary(0, 1000,);
 }
+
+function deadPhog(x, y, animation, pScope) {
+
+
+  scale(0.45);
+  var deadPhogx = animation.wave(1) * 500
+  pScope.draw_image("tadfog2", deadPhogx, y);
+
+}
+
+
+ 
+function fly (x,y,animation, pScope)  {
+
+var flyx = animation.frame * 20
+pScope.draw_image("fly", flyx, -600 -10 *animation.wave());
+}
+
+
 
 function phog(x, y, animation, pScope) {
 
   scale(0.65);
   var phogx = animation.wave(1) * 100
   console.log("here")
-  pScope.draw_image("phog", phogx, -700);
+  pScope.draw_image("phog", phogx, - 500 -300 *animation.wave());
 
 }
 
-function deadPhog(x, y, animation, pScope) {
 
-
-  scale(0.6);
-  var deadPhogx = animation.wave(1) * 300
-  pScope.draw_image("deadPhog", deadPhogx, y);
-
-}
-
-function squares(x, y, animation, pScope) {
-
-
-  // this is how you set up a background for a specific layer
-  //  let angleOffset = (360 / SLICE_COUNT) / 2
-  //  let backgroundArcStart = 270 - angleOffset;
-  //  let backgroundArcEnd = 270 + angleOffset;
-
-  //  fill(66, 135, 245)
-  //  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  //  fill(255)
-  // rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
-}
 
 function lillypad(x, y, animation, pScope) {
   if (animation.frame == 0) {
-    fill("#7d47bf")
-  ellipse (0,0,1600)
-  fill("#a05fb8")
-  ellipse (0,0,1200)
+
+    noStroke()
     fill("#437836")
     arc(x, y, 900, 800, 20, 360); // slit in lillypad
   }
+}
+
+function water(x, y, animation, pScope) {
+  if (animation.frame == 0) {
+    fill("#509bc7")
+    noStroke()
+  ellipse (0,0,1600)
+  fill("#73c1c7")
+  ellipse (0,0,1200)
+  }
+}
+
+function corn(x, y, animation, pScope){
+  fill("#7ea63f");
+  noStroke()
+  push();
+  translate(x, y-900);
+  rotate(360*animation.frame);
+  arc(x, y, 150, 150, 20, 340 );
+  pop();
 }
